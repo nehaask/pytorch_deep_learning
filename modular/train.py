a8280172ff3252be26bb729063711ff175abf3cd
Neha Kulkarni <nk4349@rit.edu>
2025-09-20 21:39:42 -0700

    configured to use mps as device

--- modular/train.py
+++ modular/train.py
@@ -1,11 +1,10 @@
 """
 Trains a PyTorch image classification model using device-agnostic code.
 """
-
+from multiprocessing import freeze_support
 import os
 import torch
 import data_setup, engine, model_builder, utils
-
 from torchvision import transforms
 
 # Setup hyperparameters
@@ -18,45 +17,56 @@ LEARNING_RATE = 0.001
 train_dir = "data/pizza_steak_sushi/train"
 test_dir = "data/pizza_steak_sushi/test"
 
-# Setup target device
-device = "cuda" if torch.cuda.is_available() else "cpu"
+def main():
+    # Setup device-agnostic code 
+    if torch.cuda.is_available():
+        device = "cuda" # NVIDIA GPU
+    elif torch.backends.mps.is_available():
+        device = "mps" # Apple GPU
+    else:
+        device = "cpu" # Defaults to CPU if NVIDIA GPU/Apple GPU aren't available
+    print(device)
 
-# Create transforms
-data_transform = transforms.Compose([
-  transforms.Resize((64, 64)),
-  transforms.ToTensor()
-])
+    # Create transforms
+    data_transform = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor()
+    ])
 
-# Create DataLoaders with help from data_setup.py
-train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
-    train_dir=train_dir,
-    test_dir=test_dir,
-    transform=data_transform,
-    batch_size=BATCH_SIZE
-)
+    # Create DataLoaders with help from data_setup.py
+    train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
+        train_dir=train_dir,
+        test_dir=test_dir,
+        transform=data_transform,
+        batch_size=BATCH_SIZE
+    )
 
-# Create model with help from model_builder.py
-model = model_builder.TinyVGG(
-    input_shape=3,
-    hidden_units=HIDDEN_UNITS,
-    output_shape=len(class_names)
-).to(device)
+    # Create model with help from model_builder.py
+    model = model_builder.TinyVGG(
+        input_shape=3,
+        hidden_units=HIDDEN_UNITS,
+        output_shape=len(class_names)
+    ).to(device)
 
-# Set loss and optimizer
-loss_fn = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(),
-                             lr=LEARNING_RATE)
+    # Set loss and optimizer
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(),
+                                lr=LEARNING_RATE)
 
-# Start training with help from engine.py
-engine.train(model=model,
-             train_dataloader=train_dataloader,
-             test_dataloader=test_dataloader,
-             loss_fn=loss_fn,
-             optimizer=optimizer,
-             epochs=NUM_EPOCHS,
-             device=device)
+    # Start training with help from engine.py
+    engine.train(model=model,
+                train_dataloader=train_dataloader,
+                test_dataloader=test_dataloader,
+                loss_fn=loss_fn,
+                optimizer=optimizer,
+                epochs=NUM_EPOCHS,
+                device=device)
 
-# Save the model with help from utils.py
-utils.save_model(model=model,
-                 target_dir="models",
-                 model_name="05_going_modular_script_mode_tinyvgg_model.pth")
+    # Save the model with help from utils.py
+    utils.save_model(model=model,
+                    target_dir="models",
+                    model_name="05_going_modular_script_mode_tinyvgg_model.pth")
+
+if __name__ == "__main__":
+    freeze_support()   # only needed if making a frozen executable (e.g., PyInstaller)
+    main()
